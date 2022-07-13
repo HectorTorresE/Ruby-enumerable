dfe8e32c8f8f00fa2a7af32a3b19386e79f735be
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-13 11:59:46 -0500

    Add any and filter methods

--- enumerable.rb
+++ enumerable.rb
@@ -5,5 +5,16 @@ module MyEnumerable
     @return
   end
 
- 
-end
+  def any? 
+    @return = false 
+    @list.each { |x| @return = true if yield x } 
+    @return 
+  end 
+
+  def filter
+    @fillter_array = [] 
+    @list.each { |x| @fillter_array << x if yield x } 
+    @fillter_array 
+  end 
+
+ end
